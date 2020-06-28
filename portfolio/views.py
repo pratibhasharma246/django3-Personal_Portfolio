437f80401339e5c9dc4b118bb230117be62d1b41
vishal <vishal@git.com>
2020-06-29 01:57:36 +0530

    First commit!

---
+++ portfolio/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+def home(request):
+    Projects = Project.objects.all()
+    return render(request,'portfolio/home.html',{'Projects':Projects})
